dab292f40996d1b367d64a1c125d65a3fb449a46
Zachary <zacharycarpenter@my.unt.edu>
2021-05-12 17:16:36 -0500

    Major change, added page navigation, template for content page, other.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // Imports
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 const port = 5000
 
-//Static Files
+//Static Files + allow page navigation within /views
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Set View's
+// Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-//Render index.ejs
+//Render index.ejs first
 app.get('', (req, res) => {
-    res.render('index', { text: 'Hey' })
+    res.render('index' )
+})
+
+app.get('/index', (req, res) => {
+    res.render('index' )
+})
+
+app.get('/postTemplate', (req, res) => {
+    res.render('postTemplate' )
 })
 
 // Listen on Port 5000
