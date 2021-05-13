c8aa26d9e944407bd9f2b288807dde781352910c
zsc0012 <zacharycarpenter@my.unt.edu>
2021-05-13 15:13:32 -0500

    Small changes

--- public/js/main.js
+++ public/js/main.js
@@ -46,7 +46,57 @@ scrollnimates.forEach( function ( sn ) {
 
     scrollnimates.forEach( function ( sn ) {
       //sn.animationOffsets == main.he
-      if ( scrollPoint > ( sn.animationOffset - halfWindowHeight * 2 ) && scrollPoint < ( sn.animationOffset + halfWindowHeight ) ) {
+      if ( scrollPoint > ( sn.animationOffset - halfWindowHeight * 2 ) && scrollPoint < (400) ) {  //Test this on other viewports
+        var magicPoint = ( scrollPoint - ( sn.animationOffset - halfWindowHeight ) ) / sn.magicNumber;
+        var up = magicPoint  + 'px';
+
+        sn.style.webkitTransform = 'translateY('+up+')';
+        sn.style.MozTransform = 'translateY('+up+')';
+        sn.style.msTransform = 'translateY('+up+')';
+        sn.style.OTransform = 'translateY('+up+')';
+        sn.style.transform = 'translateY('+up+')';
+      }
+
+    } );
+
+    window.requestAnimationFrame(step);
+
+  }
+
+ 
+  window.requestAnimationFrame(step);
+  
+})();
+
+// Parallax function 1 (modified stop point)
+(function() {
+
+var start = null;
+var scrollPosition = window.scrollY;
+var halfWindowHeight = window.innerHeight / 10; //Change this to change position of paralaxed image
+var rAFstarted = false;
+
+var scrollnimates = [].slice.call(document.getElementsByClassName('scrollnimate3'));
+// get their offset from top of screen and their scroll speed
+scrollnimates.forEach( function ( sn ) {
+    var clientOffsets = sn.getBoundingClientRect();
+    sn.animationOffset = clientOffsets.top + scrollPosition;
+    sn.magicNumber = sn.dataset.magicNumber || sn.getAttribute("data-magic-number") || "-14";
+} );
+
+/*
+   * The rAF function
+  */
+  function step(timestamp) {
+
+    if (!start) start = timestamp;
+    // full progress indicator
+    var progress = timestamp - start;
+    var scrollPoint = window.scrollY;
+
+    scrollnimates.forEach( function ( sn ) {
+      //sn.animationOffsets == main.he
+      if ( scrollPoint > ( sn.animationOffset - halfWindowHeight * 2 ) && scrollPoint < (400) ) {  //Test this on other viewports
         var magicPoint = ( scrollPoint - ( sn.animationOffset - halfWindowHeight ) ) / sn.magicNumber;
         var up = magicPoint  + 'px';
 
